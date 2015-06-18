3703c4373a75e43cbe4e3b85f4497484eaf833d8
Kosmonaft <pawel.christow@gmail.com>
2015-06-18 22:11:10 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+</div>
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<footer>
+    <?php if (is_category('2')) { ?>
+        <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/tabs.js"></script>
+    <?php } ?>
+    <?php wp_footer(); ?>
+</footer>
+
+</body>
+</html>
